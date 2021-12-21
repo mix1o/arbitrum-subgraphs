a6a06e57e37aee88a05b523339ea00ad4689c15d
Mikołaj Tęcza <mikolaj.tecza@rumblefish.pl>
2021-12-21 17:02:41 +0100

    test

--- packages/layer2-token-gateway/src/mapping.ts
+++ packages/layer2-token-gateway/src/mapping.ts
@@ -1,25 +1,32 @@
 import { GatewaySet as GatewaySetEvent } from "../generated/L2GatewayRouter/L2GatewayRouter";
 import { L2ToL1Transaction as L2ToL1TransactionEvent } from "../generated/ArbSys/ArbSys";
-import { L2ArbitrumGateway } from "../generated/templates"
-import { 
+import { L2ArbitrumGateway } from "../generated/templates";
+import {
   WithdrawalInitiated as WithdrawalInitiatedEvent,
   DepositFinalized as DepositFinalizedEvent,
-} from "../generated/templates/L2ArbitrumGateway/L2ArbitrumGateway"
-import { Gateway, L2ToL1Transaction, Token, TokenGatewayJoinTable, Withdrawal } from "../generated/schema";
+} from "../generated/templates/L2ArbitrumGateway/L2ArbitrumGateway";
+import {
+  Deposit,
+  Gateway,
+  L2ToL1Transaction,
+  Token,
+  TokenGatewayJoinTable,
+  Withdrawal,
+} from "../generated/schema";
 import { Address, BigInt, log } from "@graphprotocol/graph-ts";
 
-const bigIntToId = (input: BigInt): string => input.toHexString()
+const bigIntToId = (input: BigInt): string => input.toHexString();
 
 export const addressToId = (input: Address): string =>
   input.toHexString().toLowerCase();
 
 export const getJoinId = (gatewayId: string, tokenId: string): string =>
-  gatewayId.concat(tokenId)
+  gatewayId.concat(tokenId);
 
 export function handleGatewaySet(event: GatewaySetEvent): void {
   const gatewayId = addressToId(event.params.gateway);
   const tokenId = addressToId(event.params.l1Token);
-  const joinId = getJoinId(gatewayId, tokenId)
+  const joinId = getJoinId(gatewayId, tokenId);
 
   if (event.params.gateway == Address.zero()) {
     // TODO: handle gateways being deleted
@@ -27,16 +34,16 @@ export function handleGatewaySet(event: GatewaySetEvent): void {
   }
   // TODO: should we always create instead of load? should be faster.
   let gatewayEntity = Gateway.load(gatewayId);
-  if(gatewayEntity == null) {
+  if (gatewayEntity == null) {
     gatewayEntity = new Gateway(gatewayId);
     gatewayEntity.save();
     // we want to track every new arbitrum gateway
     // so we initialize a Data Source Template
-    L2ArbitrumGateway.create(event.params.gateway)
+    L2ArbitrumGateway.create(event.params.gateway);
   }
 
   let tokenEntity = Token.load(tokenId);
-  if(tokenEntity == null) {
+  if (tokenEntity == null) {
     tokenEntity = new Token(tokenId);
     // TODO: query gateway for L2 address
     // tokenEntity.l2Address = null;
@@ -50,25 +57,38 @@ export function handleGatewaySet(event: GatewaySetEvent): void {
 }
 
 export function handleWithdrawal(event: WithdrawalInitiatedEvent): void {
-  const withdrawalId = bigIntToId(event.params._l2ToL1Id)
-  const withdrawal = new Withdrawal(withdrawalId)
+  const withdrawalId = bigIntToId(event.params._l2ToL1Id);
+  const withdrawal = new Withdrawal(withdrawalId);
 
-  withdrawal.l2BlockNum = event.block.number
-  withdrawal.from = event.params._from
-  withdrawal.to = event.params._to
-  withdrawal.amount = event.params._amount
-  withdrawal.exitNum = event.params._exitNum
+  withdrawal.l2BlockNum = event.block.number;
+  withdrawal.from = event.params._from;
+  withdrawal.to = event.params._to;
+  withdrawal.amount = event.params._amount;
+  withdrawal.exitNum = event.params._exitNum;
 
-  const gatewayId = addressToId(event.address)
-  const tokenId = addressToId(event.params.l1Token)
-  withdrawal.exitInfo = getJoinId(gatewayId, tokenId)
-  withdrawal.l2ToL1Event = withdrawalId
+  const gatewayId = addressToId(event.address);
+  const tokenId = addressToId(event.params.l1Token);
+  withdrawal.exitInfo = getJoinId(gatewayId, tokenId);
+  withdrawal.l2ToL1Event = withdrawalId;
 
-  withdrawal.save()
+  withdrawal.save();
 }
 
 export function handleDeposit(event: DepositFinalizedEvent): void {
   // TODO: add deposit support
+  const depositId = event.transaction.hash.toHex();
+  const deposit = new Deposit(depositId);
+
+  deposit.blockNum = event.block.number;
+  deposit.from = event.params._from;
+  deposit.to = event.params._to;
+  deposit.amount = event.params._amount;
+
+  const testGatewayId = addressToId(event.address);
+  const testTokenId = addressToId(event.params.l1Token);
+  deposit.exitInfo = getJoinId(testGatewayId, testTokenId);
+
+  deposit.save();
   // TODO: add deposit event handler for template data source that tracks withdrawals
 
   // Right now this method is mimic'ing the GatewaySet handler to cover for permissionless token bridging
@@ -78,26 +98,26 @@ export function handleDeposit(event: DepositFinalizedEvent): void {
 
   const stdGatewayAddr = event.transaction.to;
 
-  if(!stdGatewayAddr) {
+  if (!stdGatewayAddr) {
     // shouldn't happen, but lets make the compiler happy
     return;
   }
 
   const gatewayId = addressToId(stdGatewayAddr);
   const tokenId = addressToId(event.params.l1Token);
-  const joinId = getJoinId(gatewayId, tokenId)
+  const joinId = getJoinId(gatewayId, tokenId);
 
   // TODO: should we always create instead of load? should be faster.
-  // the issue here is if creating again on subsequent deposits. would that break FKs? 
+  // the issue here is if creating again on subsequent deposits. would that break FKs?
   let gatewayEntity = Gateway.load(gatewayId);
-  if(gatewayEntity == null) {
+  if (gatewayEntity == null) {
     gatewayEntity = new Gateway(gatewayId);
     gatewayEntity.save();
     // we don't start a new data source template, since this is not dynamic
   }
 
   let tokenEntity = Token.load(tokenId);
-  if(tokenEntity == null) {
+  if (tokenEntity == null) {
     tokenEntity = new Token(tokenId);
     // TODO: query gateway for L2 address
     // tokenEntity.l2Address = null;
@@ -112,7 +132,7 @@ export function handleDeposit(event: DepositFinalizedEvent): void {
 
 export function handleL2ToL1Transaction(event: L2ToL1TransactionEvent): void {
   // TODO: delete L2 to L1 txs that arent a token withdrawal
-  const id = bigIntToId(event.params.uniqueId)
+  const id = bigIntToId(event.params.uniqueId);
   let entity = new L2ToL1Transaction(id);
   entity.caller = event.params.caller;
   entity.destination = event.params.destination;
@@ -123,7 +143,7 @@ export function handleL2ToL1Transaction(event: L2ToL1TransactionEvent): void {
   entity.timestamp = event.params.timestamp;
   entity.callvalue = event.params.callvalue;
   entity.data = event.params.data;
-  entity.withdrawal = id
+  entity.withdrawal = id;
 
   // TODO: query for L2 to L1 tx proof
   // TODO: don't make this an archive query
--- packages/layer2-token-gateway/tests/weth/weth.test.ts
+++ packages/layer2-token-gateway/tests/weth/weth.test.ts
@@ -1,151 +1,218 @@
-import { addressToId, getJoinId, handleGatewaySet, handleDeposit } from "../../src/mapping";
+import {
+  addressToId,
+  getJoinId,
+  handleGatewaySet,
+  handleDeposit,
+} from "../../src/mapping";
 
-import { Address, BigInt, Bytes, ethereum, store, log } from "@graphprotocol/graph-ts";
-import { newMockEvent, test, assert, createMockedFunction } from "matchstick-as";
+import {
+  Address,
+  BigInt,
+  Bytes,
+  ethereum,
+  store,
+  log,
+} from "@graphprotocol/graph-ts";
+import {
+  newMockEvent,
+  test,
+  assert,
+  createMockedFunction,
+} from "matchstick-as";
 import { GatewaySet as GatewaySetEvent } from "../../generated/L2GatewayRouter/L2GatewayRouter";
 import { DepositFinalized as DepositFinalizedEvent } from "../../generated/templates/L2ArbitrumGateway/L2ArbitrumGateway";
 import { Gateway, Token, TokenGatewayJoinTable } from "../../generated/schema";
 
-const DISABLED_GATEWAY_ADDR = Address.fromString("0x0000000000000000000000000000000000000001");
+const DISABLED_GATEWAY_ADDR = Address.fromString(
+  "0x0000000000000000000000000000000000000001"
+);
 
-const createGatewaySet = (token: Address, gateway: Address): GatewaySetEvent => {
-    let mockEvent = newMockEvent();
-  
-    let parameters = new Array<ethereum.EventParam>();
-    let tokenParam = new ethereum.EventParam("l1Token", ethereum.Value.fromAddress(token));
-    let gatewayParam = new ethereum.EventParam("gateway", ethereum.Value.fromAddress(gateway));
-    
-    parameters.push(tokenParam);
-    parameters.push(gatewayParam);
-  
-    let newGatewayEvent = new GatewaySetEvent(mockEvent.address, mockEvent.logIndex, mockEvent.transactionLogIndex,
-        mockEvent.logType, mockEvent.block, mockEvent.transaction, parameters)
-  
-    return newGatewayEvent
-}
+const createGatewaySet = (
+  token: Address,
+  gateway: Address
+): GatewaySetEvent => {
+  let mockEvent = newMockEvent();
+
+  let parameters = new Array<ethereum.EventParam>();
+  let tokenParam = new ethereum.EventParam(
+    "l1Token",
+    ethereum.Value.fromAddress(token)
+  );
+  let gatewayParam = new ethereum.EventParam(
+    "gateway",
+    ethereum.Value.fromAddress(gateway)
+  );
+
+  parameters.push(tokenParam);
+  parameters.push(gatewayParam);
+
+  let newGatewayEvent = new GatewaySetEvent(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    parameters
+  );
+
+  return newGatewayEvent;
+};
 
 const createDisabledGatewayEvent = (token: Address): GatewaySetEvent => {
-    return createGatewaySet(token, DISABLED_GATEWAY_ADDR)
-}
+  return createGatewaySet(token, DISABLED_GATEWAY_ADDR);
+};
 
-const createDepositFinalized = (token: Address, gateway: Address): DepositFinalizedEvent => {
-    let mockEvent = newMockEvent();
-  
-    let parameters = new Array<ethereum.EventParam>();
-    let tokenParam = new ethereum.EventParam("l1Token", ethereum.Value.fromAddress(token));
-    // let fromParam = new ethereum.EventParam("_from", ethereum.Value.fromAddress(from));
-    // let toParam = new ethereum.EventParam("_to", ethereum.Value.fromAddress(to));
-    // let amountParam = new ethereum.EventParam("_amount", ethereum.Value.fromI32(amount));
-    
-    parameters.push(tokenParam);
-    // parameters.push(fromParam);
-    // parameters.push(toParam);
-    // parameters.push(amountParam);
-    
-    let tx = mockEvent.transaction
-    tx.to = gateway
+const createDepositFinalized = (
+  token: Address,
+  gateway: Address,
+  from: Address,
+  to: Address,
+  amount: BigInt
+): DepositFinalizedEvent => {
+  let mockEvent = newMockEvent();
 
-    let newDepositFinalized = new DepositFinalizedEvent(mockEvent.address, mockEvent.logIndex, mockEvent.transactionLogIndex,
-        mockEvent.logType, mockEvent.block, tx, parameters)
-  
-    return newDepositFinalized
-}
+  let parameters = new Array<ethereum.EventParam>();
+  let tokenParam = new ethereum.EventParam(
+    "l1Token",
+    ethereum.Value.fromAddress(token)
+  );
+  let fromParam = new ethereum.EventParam(
+    "_from",
+    ethereum.Value.fromAddress(from)
+  );
+  let toParam = new ethereum.EventParam("_to", ethereum.Value.fromAddress(to));
+  let amountParam = new ethereum.EventParam(
+    "_amount",
+    ethereum.Value.fromSignedBigInt(amount)
+  );
+
+  parameters.push(tokenParam);
+  parameters.push(fromParam);
+  parameters.push(toParam);
+  parameters.push(amountParam);
+
+  let tx = mockEvent.transaction;
+  tx.to = gateway;
+
+  let newDepositFinalized = new DepositFinalizedEvent(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    tx,
+    parameters
+  );
+
+  return newDepositFinalized;
+};
 
 test("Can process gateway set event followed by deposit", () => {
-    const tokenAddr = Address.fromString("0xc778417e063141139fce010982780140aa0cd5ab")
-    const gatewayAddr = Address.fromString("0xf94bc045c4e926cc0b34e8d1c41cd7a043304ac9")
-    
-    let newGatewaySetEvent1 = createGatewaySet(tokenAddr, gatewayAddr)
-    handleGatewaySet(newGatewaySetEvent1)
+  const tokenAddr = Address.fromString(
+    "0xc778417e063141139fce010982780140aa0cd5ab"
+  );
+  const gatewayAddr = Address.fromString(
+    "0xf94bc045c4e926cc0b34e8d1c41cd7a043304ac9"
+  );
 
+  const toAddr = Address.fromString(
+    "0xcB3c4f5F817604f3c8B5C7e1Ecab480279C449d0"
+  );
+  const fromAddr = Address.fromString(
+    "0xcB3c4f5F817604f3c8B5C7e1Ecab480279C449d0"
+  );
 
-    const joinTableId = getJoinId(
-        addressToId(gatewayAddr),
-        addressToId(tokenAddr)
-    )
+  const amount = BigInt.fromString("10000");
 
-    let joinTableRes1 = TokenGatewayJoinTable.load(joinTableId)
-    if(!joinTableRes1) throw new Error("No join table found")
+  let newGatewaySetEvent1 = createGatewaySet(tokenAddr, gatewayAddr);
+  handleGatewaySet(newGatewaySetEvent1);
 
-    assert.stringEquals(
-        joinTableRes1.gateway.toString(),
-        gatewayAddr.toHexString()
-    )
-    assert.stringEquals(
-        joinTableRes1.token.toString(),
-        tokenAddr.toHexString()
-    )
-    
-    let newDepositFinalizedEvent1 = createDepositFinalized(tokenAddr, gatewayAddr);
-    handleDeposit(newDepositFinalizedEvent1);
-    
-    let joinTableRes2 = TokenGatewayJoinTable.load(joinTableId)
-    if(!joinTableRes2) throw new Error("No join table found")
+  const joinTableId = getJoinId(
+    addressToId(gatewayAddr),
+    addressToId(tokenAddr)
+  );
 
-    assert.stringEquals(
-        joinTableRes2.gateway.toString(),
-        gatewayAddr.toHexString()
-    )
-    assert.stringEquals(
-        joinTableRes2.token.toString(),
-        tokenAddr.toHexString()
-    )
-})
+  let joinTableRes1 = TokenGatewayJoinTable.load(joinTableId);
+  if (!joinTableRes1) throw new Error("No join table found");
 
-test("Can disable then enable gateway", () => {
-    const tokenAddr = Address.fromString("0xc778417e063141139fce010982780140aa065431")
-    const gatewayAddr = Address.fromString("0xf94bc045c4e926cc0b34e8d1c41cd7a0145156a5")
-    const joinTableId = getJoinId(
-        addressToId(gatewayAddr),
-        addressToId(tokenAddr)
-        )
-    const disabledJoinTableId = getJoinId(
-        addressToId(DISABLED_GATEWAY_ADDR),
-        addressToId(tokenAddr)
-    )
-    
-    let newGatewaySetEvent0 = createDisabledGatewayEvent(tokenAddr)
-    handleGatewaySet(newGatewaySetEvent0)
+  assert.stringEquals(
+    joinTableRes1.gateway.toString(),
+    gatewayAddr.toHexString()
+  );
+  assert.stringEquals(joinTableRes1.token.toString(), tokenAddr.toHexString());
 
-    let joinTableRes0 = TokenGatewayJoinTable.load(disabledJoinTableId)
-    if(!joinTableRes0) throw new Error("No join table found")
+  let newDepositFinalizedEvent1 = createDepositFinalized(
+    tokenAddr,
+    gatewayAddr,
+    fromAddr,
+    toAddr,
+    amount
+  );
+  handleDeposit(newDepositFinalizedEvent1);
 
-    assert.stringEquals(
-        joinTableRes0.gateway.toString(),
-        DISABLED_GATEWAY_ADDR.toHexString()
-    )
+  let joinTableRes2 = TokenGatewayJoinTable.load(joinTableId);
+  if (!joinTableRes2) throw new Error("No join table found");
 
-    assert.stringEquals(
-        joinTableRes0.token.toString(),
-        tokenAddr.toHexString()
-    )
-    let newGatewaySetEvent1 = createGatewaySet(tokenAddr, gatewayAddr)
-    handleGatewaySet(newGatewaySetEvent1)
+  assert.stringEquals(
+    joinTableRes2.gateway.toString(),
+    gatewayAddr.toHexString()
+  );
+  assert.stringEquals(joinTableRes2.token.toString(), tokenAddr.toHexString());
+});
 
-    let joinTableRes1 = TokenGatewayJoinTable.load(joinTableId)
-    if(!joinTableRes1) throw new Error("No join table found")
+// test("Can disable then enable gateway", () => {
+//   const tokenAddr = Address.fromString(
+//     "0xc778417e063141139fce010982780140aa065431"
+//   );
+//   const gatewayAddr = Address.fromString(
+//     "0xf94bc045c4e926cc0b34e8d1c41cd7a0145156a5"
+//   );
+//   const joinTableId = getJoinId(
+//     addressToId(gatewayAddr),
+//     addressToId(tokenAddr)
+//   );
+//   const disabledJoinTableId = getJoinId(
+//     addressToId(DISABLED_GATEWAY_ADDR),
+//     addressToId(tokenAddr)
+//   );
 
-    assert.stringEquals(
-        joinTableRes1.gateway.toString(),
-        gatewayAddr.toHexString()
-    )
-    assert.stringEquals(
-        joinTableRes1.token.toString(),
-        tokenAddr.toHexString()
-    )
-    
-    let newDepositFinalizedEvent1 = createDepositFinalized(tokenAddr, gatewayAddr);
-    handleDeposit(newDepositFinalizedEvent1);
-    
-    let joinTableRes2 = TokenGatewayJoinTable.load(joinTableId)
-    if(!joinTableRes2) throw new Error("No join table found")
+//   let newGatewaySetEvent0 = createDisabledGatewayEvent(tokenAddr);
+//   handleGatewaySet(newGatewaySetEvent0);
 
-    assert.stringEquals(
-        joinTableRes2.gateway.toString(),
-        gatewayAddr.toHexString()
-    )
-    assert.stringEquals(
-        joinTableRes2.token.toString(),
-        tokenAddr.toHexString()
-    )
-})
+//   let joinTableRes0 = TokenGatewayJoinTable.load(disabledJoinTableId);
+//   if (!joinTableRes0) throw new Error("No join table found");
+
+//   assert.stringEquals(
+//     joinTableRes0.gateway.toString(),
+//     DISABLED_GATEWAY_ADDR.toHexString()
+//   );
+
+//   assert.stringEquals(joinTableRes0.token.toString(), tokenAddr.toHexString());
+//   let newGatewaySetEvent1 = createGatewaySet(tokenAddr, gatewayAddr);
+//   handleGatewaySet(newGatewaySetEvent1);
+
+//   let joinTableRes1 = TokenGatewayJoinTable.load(joinTableId);
+//   if (!joinTableRes1) throw new Error("No join table found");
+
+//   assert.stringEquals(
+//     joinTableRes1.gateway.toString(),
+//     gatewayAddr.toHexString()
+//   );
+//   assert.stringEquals(joinTableRes1.token.toString(), tokenAddr.toHexString());
+
+//   let newDepositFinalizedEvent1 = createDepositFinalized(
+//     tokenAddr,
+//     gatewayAddr
+//   );
+//   handleDeposit(newDepositFinalizedEvent1);
+
+//   let joinTableRes2 = TokenGatewayJoinTable.load(joinTableId);
+//   if (!joinTableRes2) throw new Error("No join table found");
+
+//   assert.stringEquals(
+//     joinTableRes2.gateway.toString(),
+//     gatewayAddr.toHexString()
+//   );
+//   assert.stringEquals(joinTableRes2.token.toString(), tokenAddr.toHexString());
+// });
